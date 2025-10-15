658adc8828f4ff2a43284a36cab43a3acae0fa8a
Mohammadjavad Salehi <83529512+MohammadjavadSalehi@users.noreply.github.com>
2025-10-16 01:48:02 +0330

    run prettier

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
-const add = require('../index');
-test('adds 2 + 3 = 5', () => {
+const add = require("../index");
+test("adds 2 + 3 = 5", () => {
   expect(add(2, 3)).toBe(5);
 });
-test('adds 4 + 6 = 10', () => {
+test("adds 4 + 6 = 10", () => {
   expect(add(4, 6)).toBe(10);
 });
